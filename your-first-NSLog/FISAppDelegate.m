2f181b1051ebee24395faa2393ef0a7cc3396131
tgrander <treygranderson@gmail.com>
2016-04-20 13:37:49 -0400

    Done.

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -6,11 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    /**
-    
-     * Hey! Write your code here! :D
-     
-     */
+    NSLog(@ "Hello, World!");
     
     // do not alter
     return YES;  //
